11207ab06d15a8870701de2b239ec5f6d85c3685
Petra Vos <petravos70@gmail.com>
2017-11-27 20:12:18 +0100

    feat: added the Interview Question to app as a comment

--- cow-clicker/src/App.js
+++ cow-clicker/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import cowsvg from "./Cow_cartoon_04.svg";
 import "./App.css";
 
+/*Create a simple webpage that has a cow image in the middle (centered horizontally on the page) and a counter label below it.
+ Add the necessary code so that every time you click the cow image, the counter is incremented by 1.
+  The counter should start with a value of 0. You should include a brief explanation of your code.
+  Also, here is a URL for a cow image, https://upload.wikimedia.org/wikipedia/commons/2/21/Cow_cartoon_04.svg,
+  if you would like to include it in your answer.*/
+
 class App extends Component {
   //Initialize the count to be 0.
   state = {
